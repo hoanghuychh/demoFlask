d8fedc383e071cdac5bcacc816bbc637d80725e4
Cao Hoàng Huy <hoanghuychh@gmail.com>
2022-06-27 17:13:04 +0700

    fix port

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def post():
 if __name__ == '__main__':
     # for deployment
     # to make it work for both production and development
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
